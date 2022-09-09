fa38103b3eaae993f071f6871156a76f66e72b99
Mwarstep <mvoi811@gmail.com>
2022-09-09 11:12:23 -0700

    Commit 10 - Combinations_Comb3 file

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - will print all possible combinations of two diff digits,
+ * in ascending order, separated by a comma then a space.
+ *
+ * Return: 0 for a successful prog run.
+ */
+int main(void)
+{
+	int digit1, digit2;
+
+	for (digit1 = 0; digit < 9; digit1++)
+	{
+		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
+			putchar((digit1 % 10) + '0');
+			putchar((digit2 % 10) + '0');
+
+			if (digit1 == 8 && digit2 == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
+	}
+}
+putchar('\n');
+
+return (0);
