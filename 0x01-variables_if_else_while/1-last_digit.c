ff1f2ec0c86dc9344df9ab734a56055a44bb7998
Mwarstep <mvoi811@gmail.com>
2022-09-09 05:09:36 -0700

    Commit 1 - Updated v.3

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,29 +3,22 @@
 #include <stdio.h>
 
 /**
- * main - will print the last digit of a random gen number
- * and whether it's greater than 5, less than 6 or 0.
- *
- * Return: 0 for a successful program run.
- */
+ *  * main - prints the last digit of the random
+ *   * number stored in the variable n
+ *    * Return: Always 0 (Success)
+ *     */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
-	if ((n % 10) > 5)
-	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
-	}
-	else if ((n % 10) < 6 && (n % 10) != 10)
-	{
-	printf("Last digit of %d is %d and is less than 6 and is 0\n", n, n % 10);
-	}
-	else
-	{
-		printf("Last digit of %d is %d and is 0\n", n, n % 10);
-	}
+	printf("Last digit of %d is %d ", n, n % 10);
+	if (n % 10 > 5)
+		printf("and is greater than 5\n");
+	else if (n % 10 == 0)
+		printf("and is 0\n");
+	else if (n % 10 < 6 && n % 10 != 0)
+		printf("and is less than 6 and not 0\n");
 	return (0);
 }
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,1 +1,4 @@
 0-positive_or_negative - Assigns a radnom number to the variable n each time it is executed.
+
+1-last_digit - Assigns a random number to the variable n each time it's executed.
+
