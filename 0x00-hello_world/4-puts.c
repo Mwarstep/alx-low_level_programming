87d62e6138dd1778496306823e8856dcff4b14b1
Mwarstep <mvoi811@gmail.com>
2022-09-08 23:10:30 -0700

    Commit 4 - Hello, puts UPDATED

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - will print the sentence "Programming is like building a multilingual puzzle"
- * Return - 0 if exited, non-zero otherwise
+ * main - prints phrase 'Programming is like building a multilingual puzzle'
+ * Return: 0 if exited, non-zero otherwise
  */
 int main(void)
 {
