7d6a02944fb8499b7653c9a9011c2b1d05de9e7c
Mwarstep <mvoi811@gmail.com>
2022-09-08 23:23:07 -0700

    Commit 4 -Updated v.3

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,9 +4,8 @@
  * main - prints phrase 'Programming is like building a multilingual puzzle'
  * Return: 0 if exited, non-zero otherwise
  */
-
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle ");
+	puts("\"Programming is like building a multiligual puzzle");
 	return (0);
 }
