7788305b737d6c0cbe8eb432caa4ad48c065de4b
Mwarstep <mvoi811@gmail.com>
2022-09-09 00:00:17 -0700

    Commit 5 - updated

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - prints phrase with proper grammar
+ * main - prints phrase with proper grammar,
  * but the outcome is a piece of art,
- * return 0 if prog success
+ * return 0 if prog successful
  */
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
