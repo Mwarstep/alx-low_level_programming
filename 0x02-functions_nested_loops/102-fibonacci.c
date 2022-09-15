acf93bb157fc3fa8f1237c9927fbd5c18e83a5f1
Mwarstep <mvoi811@gmail.com>
2022-09-15 08:09:53 -0700

    Commit 14 -Fibonacci 1

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - will print the first 50 Fibonacci numbers, starting with 1 and 2
+ * then a new line
+ * Return: 0 for a successful prog run.
+ */
+int main(void)
+{
+	long int x, y, z, next;
+
+	y = 1;
+	z = 2;
+
+	for (x = 1; x <= 50; x++)
+	{
+		if (y != 23065011074)
+		{
+			printf("%ld", y);
+		} else
+		{
+			printf("%ld\n", y);
+		}
+		next = y + z;
+		y = z;
+		z = next;
+	}
+	return (0);
+}
