158b03ba8b09ebb3574360784b699876dd7eb78c
Mwarstep <mvoi811@gmail.com>
2022-09-14 23:36:44 -0700

    Commit 12 - UPDATED

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -33,4 +33,17 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar(' ');
 					_putchar((z / 10) + '0');
-					_putchar
+					_putchar((z % 10) + '0');
+				} else if (z >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((z / 100) + '0');
+					_putchar(((z / 10) % 10) + '0');
+					_putchar((z % 10) + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
