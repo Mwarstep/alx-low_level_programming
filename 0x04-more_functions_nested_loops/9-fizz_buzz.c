752573afefe90594b6a824700e4c7620d0fbf8ea
Mwarstep <mvoi811@gmail.com>
2022-09-16 02:30:00 -0700

    Commit 9 - FizzBuzz Test v.1

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - will print the numbers from 1 to 100, then a new line
+ * for multiples of 3, it will print 'Fizz' instead of the number
+ * for multiples of 5, it will print 'Buzz'
+ *
+ * Return: 0 for a successful prog run
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 1; x <= 100; x++)
+	{
+		if (x % 3 == 0 && 5 != 0)
+		{
+			printd(" Fizz");
+		}
+		else if (x % 5 == 0 && 3 != 0)
+		{
+			printf(" Buzz");
+		}
+		else if (x % 3 == 0 && x % 5 == 0)
+		{
+			printf(" FizzBuzz");
+		}
+		else if (x == 1)
+		{
+			printf("%d", x);
+		}
+		else
+		{
+			printf(" %d", x);
+		}
+	}
+	printf("\n");
+
+	return (0);
+}
