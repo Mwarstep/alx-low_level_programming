0ed5b0534ef5e2290d9ea1bb1763b6b7a4f72a18
Mwarstep <mvoi811@gmail.com>
2022-09-19 04:43:48 -0700

    Commit 5 - Rev String

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - will reverse a string
+ * @s: stirng that will be reversed.
+ */
+
+void rev_string(char *s)
+{
+	int x, len1, len2;
+	char y;
+
+	while (s[len1] != '\0')
+	{
+		len1++;
+	}
+	len2 = len1 - 1;
+
+	for (x = 0; x < len1 / 2; x++)
+	{
+		y = s[x];
+		s[x] = s[len2];
+		s[len2--] = y;
+	}
+}
