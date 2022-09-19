ba6be10c31bcacbc58e6923102e10560458e5376
Mwarstep <mvoi811@gmail.com>
2022-09-19 03:18:23 -0700

    Commit 0 - 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - will update the value of the variable
+ * with the pointer pointing to 98
+ * @n: pointer
+ * Return: None
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
