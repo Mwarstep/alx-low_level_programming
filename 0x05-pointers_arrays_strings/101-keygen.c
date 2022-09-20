f2b1eeda9d1e531aff41dd21f44c29a971f809c8
Mwarstep <mvoi811@gmail.com>
2022-09-20 00:43:42 -0700

    Commit 11 - 101 Keygen

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - will generate random valid passwords for the prog 101-crackme
+ *
+ * Return: 0 for a succesful program run.
+ */
+
+int main(void)
+{
+	int pass[100];
+	int x, y, sum;
+
+	sum = 0;
+	srand(time(NULL));
+
+	for (x = 0; x < 100 ; x++)
+	{
+		pass[x] = rand() % 78;
+		sum += (pass[x] + '0');
+		putchar(pass[x] + '0');
+
+		if ((2772 - sum) - '0' < 78)
+		{
+			y = 2772 - sum - '0';
+			sum += y;
+
+			putchar(y + '0');
+			break;
+		}
+	}
+	return (0);
+}
