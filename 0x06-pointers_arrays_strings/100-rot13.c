f6afc5db8965657444b9b1048ad58f49fed60a61
Mwarstep <mvoi811@gmail.com>
2022-09-22 06:34:22 -0700

    Commit 8 - rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rot13 - Will encode a string in rot13
+ * @s: The string to be encoded
+ *
+ * Return: Encoded string.
+ */
+
+char *rot13(char *s)
+{
+	int x, y;
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b[] = "NOPQRSTUVWXYZABCDEFGHIJLKMnopqrstuvwxyzabcdefghijklm";
+
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		for (y = 0; a[y] != '\0'; y++)
+		{
+			if (s[x] == a[y])
+			{
+				s[x] = b[y];
+				break;
+			}
+		}
+	}
+	return (s);
+}
