45d8c1d358f8be662f1274e8aabbe5e66b4dba8e
Mwarstep <mvoi811@gmail.com>
2022-09-30 07:41:36 -0700

    Commit 1 - args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - will print the number of arguments passed into it
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 for a successfull program run
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
