bbddbb3991b594db89cfe6a8238c1f93c8ce19ee
Mwarstep <mvoi811@gmail.com>
2022-10-06 04:53:23 -0700

    Commit 4 - Realloc UPDATE #2

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -16,7 +16,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	char *pnt2;
 	unsigned int x;
 
-	if (new_size == old size)
+	if (new_size == old_size)
 		return (ptr);
 
 	if (new_size == 0 && ptr)
