f83967465a56c254f52c19dd02627df14a70c363
Mwarstep <mvoi811@gmail.com>
2022-10-11 03:58:00 -0700

    Commit 0 - Dog.h Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Dog details
+ * @name: 1st data point
+ * @age: 2nd data point
+ * @owner: 3rd data point
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
