110b7d64cc311a12a9eb4d7af9d53709aa8ba267
Mwarstep <mvoi811@gmail.com>
2022-10-11 03:14:06 -0700

    Commit 2 - File Name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - will print the name of the file
+ *
+ * Return: 0 for a successful program run
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
