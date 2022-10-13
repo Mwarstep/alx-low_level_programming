590a63c951b98b226fa015450862e0daaf331dd5
Mwarstep <mvoi811@gmail.com>
2022-10-12 23:55:42 -0700

    Commit 0 - Print Name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * print_name - will print a name.
+ * @name: The name to be printed
+ * @f: The pointer to printing function
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (!name || !f)
+		return;
+	f(name);
+}
