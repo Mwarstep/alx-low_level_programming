e043b5fc350354bfbac613638e859aef63a83f3d
Mwarstep <mvoi811@gmail.com>
2023-04-12 01:39:47 -0700

    Header File - New

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef S_ALGOS_H
+#define S_ALGOS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif /* S_ALGOS_H */
